94688e0024a3d54f6f978d0262a71eb4be719ad3
Blaz Korecic <blaz.korecic@ug.uchile.cl>
2021-04-22 00:35:13 -0400

    Add users app with User, Region and Comuna models.
    Also adds a data migration '0002_regiones_comunas', which
    loads all the Chile "regiones" and "comunas" into the db.

--- adoptapp/settings.py
+++ adoptapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +81,10 @@ DATABASES = {
     }
 }
 
+
+# Auth user model
+
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ users/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifier for
+    authentication instead of AbstractUser's default username field.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save an user with the given email and password.
+        """
+        if not email:
+            raise ValueError('The email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self.create_user(email, password, **extra_fields)
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Comuna(models.Model):
+    name = models.CharField(max_length=30)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    username = None  # Not using AbstractUser's username field
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(primary_key=True, blank=False)
+    phone_number = models.CharField(max_length=12, blank=False)
+    address = models.CharField(max_length=100, blank=False)
+    region = models.ForeignKey(Region, on_delete=models.PROTECT, blank=False)
+    comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT, blank=False)
+    description = models.CharField(max_length=260, blank=True)
+    register_date = models.DateField(auto_now_add=True)
+    birth_date = models.DateField(blank=False)
+
+    USERNAME_FIELD = 'email'  # Use 'email' field for authentication
+    REQUIRED_FIELDS = ['first_name', 'last_name',
+                       'phone_number', 'address',
+                       'region', 'comuna',
+                       'birth_date']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
