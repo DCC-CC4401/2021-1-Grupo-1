403b15989058515a4f5a05afda73f8bb224c6718
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-04-29 17:53:04 -0400

    Added LOGIN_URL as /login/

--- adoptapp/settings.py
+++ adoptapp/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
 # Auth user model
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/login/'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
