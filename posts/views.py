baeb0dd18fc709e3b4d9b3afb881944bc56453ef
nadiadek <nadiasilva@ug.uchile.cl>
2021-06-17 00:00:46 -0400

    authentication required to post

--- posts/views.py
+++ posts/views.py
@@ -7,13 +7,27 @@ from posts.forms import PostForm
 
 # Create your views here.
 def create_post_view(request):
-    if request.method == "GET":
-        form = PostForm()
-        return render(request, "posts/create_post.html", {'form':form})
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            form = PostForm()
+            return render(request, "posts/create_post.html", {'form': form})
 
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            new_post = '' #no sé que poner aqui uwu
-            return HttpResponseRedirect(reverse('home'))
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                new_post = '' #no sé que poner aqui uwu
+                return HttpResponseRedirect(reverse('home'))
+    else:
+        #Aquí se puede redirigir a una página de error quizás, por el momento esta igual que el if para trabajar
+        if request.method == "GET":
+            form = PostForm()
+            return render(request, "posts/create_post.html", {'form': form})
+
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                new_post = ''  # no sé que poner aqui uwu
+                return HttpResponseRedirect(reverse('home'))
+
