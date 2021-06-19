f6a87fed069fda816fda234b568ba7d135cbf85c
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-06-19 19:01:19 -0400

    Model fixes from create_post

--- posts/models.py
+++ posts/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 class Post(models.Model):
-    post_date = models.DateField(auto_now_add=True) 
-    specie = models.CharField(max_length = 30)
-    pet_name = models.CharField(max_length = 30)
-    author = models.ForeignKey("User", on_delete = models.CASCADE)
-    comuna = models.ForeignKey("Comuna", on_delete = models.PROTECT)
-    description = models.TextField(max_length = 1000)
-    breed = models.CharField(max_length = 50)
-    sex = models.CharField(choices = [('MA', 'Macho'), ('HE', 'Hembra')])
-    pet_size = models.CharField(choices = [('GR', 'Grande'), ('ME', 'Mediano'), ('PE', 'Pequeño')])
-    parasytes = models.CharField(choices = [('SI', 'Sí'), ('NO', 'No')])
-    sterilized =  models.CharField(choices = [('SI', 'Sí'), ('NO', 'No'), ('DS', 'Desconocido')])
-    vaccinated = models.CharField(choices = [('SI', 'Sí'), ('NO', 'No'), ('DS', 'Desconocido')])
+    post_date = models.DateField(auto_now_add=True)
+    specie = models.CharField(max_length=30)  # este al final dijimos que va a ser select o input? los modelos no calzan
+    pet_name = models.CharField(max_length=30)
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    comuna = models.ForeignKey("users.Comuna", on_delete=models.PROTECT)
+    description = models.TextField(max_length=1000)
+    breed = models.CharField(max_length=50)  # aquí pasa lo mismo que con la especie
+    sex = models.CharField(choices=[('MA', 'Macho'), ('HE', 'Hembra'), ('DS', 'Desconocido')], max_length=10)
+    pet_size = models.CharField(choices=[('GR', 'Grande'), ('ME', 'Mediano'), ('PE', 'Pequeño')], max_length=10)
+    parasytes = models.CharField(choices=[('SI', 'Sí'), ('NO', 'No')], max_length=10)
+    sterilized = models.CharField(choices=[('SI', 'Sí'), ('NO', 'No'), ('DS', 'Desconocido')], max_length=10)
+    vaccinated = models.CharField(choices=[('SI', 'Sí'), ('NO', 'No'), ('DS', 'Desconocido')], max_length=10)
+    status = models.CharField(choices=[('Mío', 'Mío'), ('Calle', 'De la calle')], max_length=15)
+    sighting_date = models.DateField(null=True)
 
 class PostImage(models.Model):
     post = models.ForeignKey(Post, on_delete = models.CASCADE)
