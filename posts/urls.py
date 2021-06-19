83b2dd34fd61a7cbcb67ada1a08af020f86f9bf1
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-06-19 18:35:20 -0400

    Modified template to use model

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/', views.post_view, name='post'),
-    #path('post/<int:post_id>', views.post_view, name='post'),
+    path('post/<int:post_id>', views.post_view, name='post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-def post_view(request): #(request, post_id)
+def post_view(request, post_id):
     if request.method == "GET":
-        return render(request, "posts/post.html")
-        #post_info = Post.objects.get(pk=post_id)
-        #return render(request, 'posts/post.html', {'post_info': post_info})
+        post_info = Post.objects.get(pk=post_id)
+        return render(request, 'posts/post.html', {'post_info': post_info})
