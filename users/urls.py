f5f1c9cb84c4f0bf55d4933becc78bdd8d4d7fab
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-06-26 17:30:36 -0400

    Fixed bad logout URL

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('api/get_comunas/<int:region_id>', views.get_comunas_view),
     path('login/', views.login_user_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('logout', views.logout_user, name='logout'),
+    path('logout/', views.logout_user, name='logout'),
     path('profile/<int:user_id>', views.profile_view, name='profile'),
     path('profile/edit/<int:user_id>', views.profile_edit, name='profile_edit'),
 ]
