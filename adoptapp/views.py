94d9d2325295c019f2e071c60793a66e2e3a0523
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-06-26 21:14:11 -0400

    Added logic to homepage

--- adoptapp/views.py
+++ adoptapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 
+from posts.models import Post, PostImage
+
 
 class HomepageView(View):
 
     def get(self, request):
-        return render(request, "adoptapp/homepage.html")
+        queryset = Post.objects.order_by("-post_date")[:10]
+        images = []
+        for p in queryset:
+            photo = PostImage.objects.filter(post_id=p.id).first()
+            images.append(photo)
+        zipped = zip(queryset, images)
+        return render(request, "adoptapp/homepage.html", {'posts': zipped})
 
 
 class ResultsView(View):
