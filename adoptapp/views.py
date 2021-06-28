085fb8360d088ca20bf0071bab14a38ef94c5c9b
Matías Vergara S <matiasvergara@ug.uchile.cl>
2021-06-27 23:53:50 -0400

    Multiple design changes in results, post and forms

--- adoptapp/views.py
+++ adoptapp/views.py
@@ -18,7 +18,7 @@ def results_view(request):
         photo = PostImage.objects.filter(post_id=p.id).first()
         images.append(photo)
     results = list(zip(results, images))
-    paginator = Paginator(results, 10)
+    paginator = Paginator(results, 3)
     page_number = request.GET.get('page', '1')
     page_obj = paginator.get_page(page_number)
     return render(request, "adoptapp/results.html", {'results': page_obj})
